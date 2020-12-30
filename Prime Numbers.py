9807bebd306fb891cb3389963728762c550c7fd3
JanithMS <60615623+JanithMS@users.noreply.github.com>
2020-12-30 10:58:32 +0530

    Create Prime Numbers.py

---
+++ Prime Numbers.py
@@ -0,0 +1,24 @@
+// getting the inputs from user
+num1 = int(input('Enter the starting number: '))
+num2 = int(input('Enter the ending number: '))
+
+//function to find the number is prime number
+def isPrime(num):
+    for i in range (2, num):
+        if(num % i == 0):
+            return 0
+    return 1
+    
+//finding the prime numbers in given range
+prime_numbers = []
+for j in range (num1, num2+1):
+    if(isPrime(j) == 1):
+        prime_numbers.append(j)
+
+//printing the number in required format
+mainString = 'Prime numbers between %d and %d are: ' % (num1, num2)
+longString = ''
+for k in prime_numbers:
+    string = str(k)
+    longString = longString+ string+ ' '
+print(mainString+longString)
